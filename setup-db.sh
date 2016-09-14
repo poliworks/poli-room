808528a4e86d90cec9788912a10bb0844a0c714e
Leonardo Iacovini <leoiacovini@gmail.com>
2016-09-14 20:57:11 -0300

    Improves setup-db.sh tooling options

--- setup-db.sh
+++ setup-db.sh
@@ -1,2 +1,16 @@
-docker rm pcs3623-postgresql
-docker run --name pcs3623-postgresql -p 5432:5432 -e POSTGRES_USER=pcs3623 -e POSTGRES_PASSWORD=pcs3623 -e POSTGRES_DB=pcs3623 -d postgres
+if [[ $1 == "delete" ]]; then
+	docker rm pcs3623-postgresql
+elif [[ $1 == "start" ]]; then
+	docker start pcs3623-postgresql
+elif [[ $1 == "stop" ]]; then
+	docker stop pcs3623-postgresql
+elif [[ $1 == "setup" ]]; then
+	docker rm pcs3623-postgresql
+	docker run --name pcs3623-postgresql -p 5432:5432 -e POSTGRES_USER=pcs3623 -e POSTGRES_PASSWORD=pcs3623 -e POSTGRES_DB=pcs3623 -d postgres
+else
+	echo "Choose one of the following options:"
+	echo "delete  -> Remove any existing instances of the database"
+	echo "start  -> Start a existing instance of the database, or create one if none exists"
+	echo "stop -> Stop a running instance of the database"
+	echo "setup -> Wipe and create a new database"
+fi
