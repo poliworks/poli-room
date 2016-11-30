41558594ebe7ed078b5ab450f6ac82a0d286f750
Leonardo Iacovini <leoiacovini@gmail.com>
2016-11-30 10:54:04 -0200

    Created new actions for Rooms

--- app/models/dao/RoomDao.scala
+++ app/models/dao/RoomDao.scala
@@ -1,10 +1,36 @@
 package models.dao
 
+import misc.json.schemas.RegisterRoomSchema
 import models.Room
+import scalikejdbc._
 
 class RoomDao {
 
   private val r = Room.syntax("r")
 
+  def findBy(attribute: String, value: String)(implicit session: DBSession): Option[Room] = {
+    withSQL {
+      select.from(Room as r).where.eq(r.column(attribute), value)
+    }.map(Room.apply).single().apply()
+  }
+
+  def findById(id: Long)(implicit session: DBSession) = findBy("id", s"$id")
+
+  def findAllFromBuilding(building: String)(implicit session: DBSession): List[Room] = {
+    withSQL {
+      select.from(Room as r).where.eq(r.building, building)
+    }.map(Room.apply).list.apply()
+  }
+
+  def registerRoom(registerRoomSchema: RegisterRoomSchema)(implicit session: DBSession): Room = {
+    val id = withSQL {
+      insert.into(Room).namedValues(
+        r.name -> registerRoomSchema.name,
+        r.department -> registerRoomSchema.department,
+        r.size -> registerRoomSchema.size,
+        r.building -> registerRoomSchema.building)
+    }.updateAndReturnGeneratedKey().apply()
+    registerRoomSchema.toRoom(id)
+  }
 
 }
