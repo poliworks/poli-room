3b84b2da098688a04a05b5f8c7b3006fcfd355cb
Leonardo Iacovini <leoiacovini@gmail.com>
2016-12-01 23:58:43 -0200

    Improve ErrorHandler message

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -26,6 +26,16 @@ class ErrorHandler @Inject()(env: Environment,
   }
 
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = Future.successful {
-    Status(statusCode)(Json.obj("message" -> message))
+    Status(statusCode)(Json.obj("message" -> messageForCode(statusCode, message)))
   }
+
+  private def messageForCode(statusCode: Int, message: String): String = {
+    statusCode match {
+      case 404 => "Not Found" + message
+      case 403 => "Forbidden" + message
+      case 400 => "Bad Request" +  message
+      case _ => message
+    }
+  }
+
 }
